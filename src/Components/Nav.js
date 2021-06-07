b2f153d1c20471cdc85683e9b7b7cdf42aa69f7b
Frédéric <frederichermans@hotmail.com>
2021-06-07 20:19:17 +0200

    registerpage

---
+++ src/Components/Nav.js
@@ -0,0 +1,11 @@
+import Container from "./Container";
+
+const Nav = () => {
+    return ( 
+        <Container>
+            
+        </Container>
+     );
+}
+ 
+export default Nav;
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -3,6 +3,9 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 import PrimaryTitle from "../Components/PrimaryTitle";
+import PrimaryText from "../Components/PrimaryText";
+import Container from "../Components/Container";
+import TextLink from "../Components/TextLink";
 
 export default function Signup() {
 	const emailRef = useRef();
@@ -33,34 +36,94 @@ export default function Signup() {
 	}
 
 	return (
-		<>
+		<Container>
 			<Card>
 				<Card.Body>
-					<PrimaryTitle title="Sign Up" color="#fff" />
-					<h2 className='text-center mb-4'>Sign Up</h2>
+					<PrimaryTitle title="Welkom." color="#fff" />
+					<PrimaryText text="Maak je nieuwe account aan en start met het plannen van je budget!" color="#fff"/>
 					{error && <Alert variant='danger'>{error}</Alert>}
 					<Form onSubmit={handleSubmit}>
 						<Form.Group id='email'>
-							<Form.Label>Email</Form.Label>
-							<Form.Control type='email' ref={emailRef} required />
+							<div className='formStyle'>
+								<Form.Label className='labelStyle'>Email</Form.Label>
+								<Form.Control className='inputStyle' type='email' ref={emailRef} required />
+							</div>
 						</Form.Group>
 						<Form.Group id='password'>
-							<Form.Label>Password</Form.Label>
-							<Form.Control type='password' ref={passwordRef} required />
+							<div className='formStyle'>
+								<Form.Label className='labelStyle'>Wachtwoord</Form.Label>
+								<Form.Control className='inputStyle' type='password' ref={passwordRef} required />
+							</div>
 						</Form.Group>
 						<Form.Group id='password-confirm'>
-							<Form.Label>Password Confirmation</Form.Label>
-							<Form.Control type='password' ref={passwordConfirmRef} required />
+							<div className='formStyle'>
+								<Form.Label className='labelStyle'>Bevestig wachtwoord</Form.Label>
+								<Form.Control className='inputStyle' type='password' ref={passwordConfirmRef} required />
+							</div>
 						</Form.Group>
-						<Button disabled={loading} className='w-100' type='submit'>
+						<Button disabled={loading} className='btnLogin' type='submit'>
 							Sign Up
 						</Button>
 					</Form>
 				</Card.Body>
 			</Card>
-			<div className='w-100 text-center mt-2'>
-				Already have an account? <Link to='/login'>Log In</Link>
-			</div>
-		</>
+			<TextLink 
+				text="Heb je al een account? " 
+				link="/Login"
+				linkLabel="Log in"
+			/>
+			<style jsx>{`
+				.formStyle {
+					width: 100%;
+					height: 60px;
+					color: #fff;
+					border: 2px solid #fff;
+					border-radius: 10px;
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+					padding: 20px;
+					margin-bottom: 13px;
+				}
+
+				.labelStyle {
+					margin-bottom: 5px;
+				}
+
+				.inputStyle {
+					width: 100%;
+					color: #fff;
+					background-color: rgba(201, 76, 76, 0);
+					border: none;
+					border-bottom: 1px solid #fff;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+				}
+
+				.btnLogin {
+					position: absolute;
+					bottom: 0;
+					right: 20px;
+					left: 20px;
+					width: 335px;
+					height: 60px;
+					color: #006fff;
+					background-color: #fff;
+					border-radius: 10px;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					margin-bottom: 20px;
+					border: none;
+				}
+
+				.wachtwoord {
+					color: #FFFFFF;
+					display: flex;
+					align-content: flex-end;
+				}
+			`}</style>
+		</Container>
 	);
 }
